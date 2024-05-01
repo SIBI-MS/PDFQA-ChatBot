4948820a4dba3c2f30fe30f81fc86c83bb315ccb
sibims <sibima00@gmail.com>
2024-05-01 12:01:08 +0530

    Entire code changed

--- app.py
+++ app.py
@@ -1,112 +1,147 @@
 import os
-import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import FAISS
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage #schemas
+from langchain_community.document_loaders import WebBaseLoader  #load content from url
+from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter #used make chunks using the contents from url
+from langchain_community.vectorstores import Chroma #to store vectors created my using the above chunks
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from sentence_transformers import SentenceTransformer
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.llms import HuggingFaceHub
-from html import css,bot,user
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from sentence_transformers import SentenceTransformer
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
- 
 load_dotenv()
 
-#Storing apikey and model name
-os.environ['HUGGINGFACE_API_KEY']=os.getenv("HUGGINGFACE_API_KEY")
-model_id = 'tiiuae/falcon-7b-instruct'
+# #Storing apikey and model name
+# os.environ['HUGGINGFACE_API_KEY']=os.getenv("HUGGINGFACE_API_KEY")
+# model_id="meta-llama/Llama-2-7b-chat-hf"
 
-def get_pdf_text(pdf_docs):
+
+def get_vectorstore_from_url(pdf_docs):
+    # get the text in document form
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
-    return text
-
-
-def get_text_chunks(text):
+    # return text
+    
+    # split the document into chunks
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text)
-    return chunks
+    # chunks = text_splitter.split_text(text)
+    document_chunks = text_splitter.split_text(text)
+    print(type(document_chunks))
 
+    
+    # create a vectorstore from the chunks
+    embeddings = HuggingFaceInstructEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+    vector_store = Chroma.from_texts(document_chunks,embeddings)
 
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+    return vector_store
 
-
-def get_conversation_chain(vectorstore):
-    model_id = 'tiiuae/falcon-7b-instruct'
-    llm = HuggingFaceHub(
-        huggingfacehub_api_token=os.environ['HUGGINGFACE_API_KEY'],
-        repo_id=model_id,
-        model_kwargs={"temperature": 0.7, "max_new_tokens": 500}
+def get_context_retriever_chain(vector_store):
+    llm = Ollama(
+        model="phi3",
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
     )
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+
+#For create a chain
+def get_conversational_rag_chain(retriever_chain): 
+    # llm = HuggingFaceHub(
+    #     huggingfacehub_api_token=os.environ['HUGGINGFACE_API_KEY'],
+    #     repo_id=model_id,
+    #     model_kwargs={"temperature": 0.3, "max_new_tokens": 500}
+    # )
+    llm = Ollama(
+        model="phi3",
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
     )
-    return conversation_chain
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain,stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="ChatWeb", page_icon="🤖")
+st.title("ChatWeb")
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    pdf_docs = st.file_uploader("Upload your PDFs", accept_multiple_files=True)
+
+    if pdf_docs is None:
+        st.info("Upload Your PDFs Here")
+
+    # elif st.button("Process"):
+    #     with st.spinner("Processing"):
+    # session state
+    if st.button("Process"):
+        with st.spinner("Processing"):
+            if "vector_store" not in st.session_state: 
+                st.session_state.vector_store = get_vectorstore_from_url(pdf_docs) 
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot. How can I help you?"),
+    ] 
 
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    response = get_response(user_query)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
+    
 
 
-def main():
-    load_dotenv()
-    st.set_page_config(page_title="PDFQA-ChatBot",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
-
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.header("PDFQA:books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your PDFs", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
-
-if __name__ == '__main__':
-    main()
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
